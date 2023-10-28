855a7e226c906e94cd72f26edb92f2a606fdeaea
Wayne-Chou <110703587+Wayne-Chou@users.noreply.github.com>
2023-10-28 16:17:15 +0800

    Update index.js

--- 13-js-day13/index.js
+++ 13-js-day13/index.js
@@ -1,17 +1,3 @@
-function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        var later = function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
-    };
-};
 
 // 取得所有圖片
 const sliderImages = document.querySelectorAll('.slide-in');
@@ -45,5 +31,5 @@ function checkSlide() {
     });
 }
 
-// window.addEventListener('scroll', debounce(checkSlide));
+
 window.addEventListener('scroll', checkSlide);
