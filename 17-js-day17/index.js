25af7f4b4cd6a978024cf5a3435e967c1aa13361
Wayne-Chou <110703587+Wayne-Chou@users.noreply.github.com>
2023-11-11 16:19:39 +0800

    Update index.js

--- 17-js-day17/index.js
+++ 17-js-day17/index.js
@@ -1,24 +1,26 @@
 // 將陣列儲存在bands變數
-// 看起來沒將an a the排除發問
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything',
-    'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+// 出處https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+const bands = ["The Plot in You", "The Devil Wears Prada", "the"];
 
 // strip funtion帶參數bandName
-// 回傳使用正則表達排除a、the、an的英文並用trim去除左右空白
+// 用trim去除左右空白
 function strip(bandName) {
-    return bandName.replace(/^(a |the |an )/i, '').trim();
+  console.log("bandName", bandName);
+  console.log(
+    "bandName.replace(/^(a |the |an )/i, '').trim()",
+    bandName.replace("Plot", "apple").replace("the", "hank").trim()
+  );
+  return bandName.replace("Plot", "apple").replace("the", "hank").trim();
 }
 
 // 這裡使用sort排序並且觸發strip funtion在排序
-const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
-
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
 
 // 最後取得html名為#bands的id選擇器
 // 並將剛剛排序好的使用map回傳新的陣列
 // join() 方法用來將陣列的所有元素按指定的分隔符號合併成一個字串。
-document.querySelector('#bands').innerHTML =
-    sortedBands
-        .map(band => `<li>${band}</li>`)
-        .join('');
+document.querySelector("#bands").innerHTML = sortedBands
+  .map((band) => `<li>${band}</li>`)
+  .join("");
 
 console.log(sortedBands);
